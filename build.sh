399fa0fbcc246dadac4c8f06165eb375e9820d40
kenneth <kennethcarbonell13@gmail.com>
2025-11-11 05:23:03 +0000

    test 1.1

--- build.sh
+++ build.sh
@@ -1,17 +1,6 @@
 echo "Building the sample project..."
 # Install dependencies
 #!/bin/bash
-set -e
-
-echo "Creating virtual environment..."
-python3 -m venv venv
-. venv/bin/activate
-
-apt update
-apt install -y python3-pip python3-pycodestyle
-
-echo "Installing dependencies..."
-pip install pycodestyle
 
 # Run the Python script to check if it works
 python3 sample.py
